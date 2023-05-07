bc9ef227d1c46f6ff34986e9eec490d941de8c86
Alexandr Rodionov <alexrodionby@gmail.com>
2023-05-07 11:04:52 +0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Alexandr Rodionov on 7.05.23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
